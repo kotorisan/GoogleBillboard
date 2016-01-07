5ea7fd8b767feb674d24700ffbd0fc9c44ea7134
unknown <stdeng1@697-334-Bard.sf.k12.internal>
2016-01-07 11:26:44 -0800

    FInished

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -17,23 +17,30 @@ public class GoogleBillboard extends PApplet {
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {            
-  String digits = e.substring(2,12);
-  double dNum = Double.parseDouble(digits);
-  System.out.println(dNum);
+  // String digits = e.substring(2,12);
+  // double miyu = Double.parseDouble(digits);
+  // System.out.println(miyu);
   noLoop();  
 }  
 public void draw()  
 {   
-	//not needed for this assignment
+	for (int ilya = 2; ilya < e.length(); ilya++)
+	{
+		double kuro = Double.parseDouble(e.substring(ilya, ilya + 10));
+		if (isPrime(kuro) == true)
+		{
+			System.out.println(e.substring(ilya, ilya + 10)+".com");
+			break;
+		}
+	}
 }  
-public boolean isPrime(double dNum)  
+public boolean isPrime(double miyu)  
 {   
-  if (dNum < 2)
+  if (miyu < 2)
   	return false;
-  for (int umi = 2; umi <= Math.sqrt(dNum); umi++)
-  	if (dNum%umi == 0)
+  for (int umi = 2; umi <= Math.sqrt(miyu); umi++)
+  	if (miyu%umi == 0)
     	return false;
-    return true;
   return true;  
 } 
   static public void main(String[] passedArgs) {
